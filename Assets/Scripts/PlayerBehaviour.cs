8f8c6a18d4c0f4726a78782c723f63942e2aadd5
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2026-04-19 19:57:59 -0500

    starter script for player sprite

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerBehaviour : MonoBehaviour
 {
     public bool isGrounded, isFacingRight, isMidAir, isTouchingWall;
+    public float jumpForce = 5f;
+    public float speed = 3f;
     void Start()
     {
         
@@ -11,6 +15,12 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Keyboard k = Keyboard.current;
+
+        if (k.spaceKey.wasPressedThisFrame && isTouchingWall) {
+            // TODO: jump from wall
+            isFacingRight = !isFacingRight;
+        }
         
     }
 }
