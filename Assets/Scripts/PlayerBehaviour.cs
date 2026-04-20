968d575e3519cd7cc0aa8cc09d935e15447f4315
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2026-04-19 19:52:54 -0500

    add basic floor, 3 walls, and player sprute

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public bool isGrounded, isFacingRight, isMidAir, isTouchingWall;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
