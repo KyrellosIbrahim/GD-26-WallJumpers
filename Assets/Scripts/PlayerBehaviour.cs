db814fb101ea8f6b44ad9648e0c21c7cee633abe
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2026-04-19 20:15:51 -0500

    basic logic for player jump and walk behavior

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,25 +2,55 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 
+public enum PlayerState { Grounded, OnWall, Airborne }
+
 public class PlayerBehaviour : MonoBehaviour
 {
-    public bool isGrounded, isFacingRight, isMidAir, isTouchingWall;
+    public PlayerState state = PlayerState.OnWall;
+    public bool isFacingRight = true;
+    public bool hasAirJump = false;
     public float jumpForce = 5f;
     public float speed = 3f;
+    public float wallJumpForce = 3f;
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Keyboard k = Keyboard.current;
+        HandleJump();
+        HandleAutoWalk();
+    }
 
-        if (k.spaceKey.wasPressedThisFrame && isTouchingWall) {
-            // TODO: jump from wall
+    public void HandleJump() {
+        Keyboard k = Keyboard.current;
+        if (!k.spaceKey.wasPressedThisFrame) return;
+
+        if (state == PlayerState.Grounded) {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            hasAirJump = true;
+            state = PlayerState.Airborne;
+        } else if (state == PlayerState.OnWall) {
             isFacingRight = !isFacingRight;
+            float xDirection = isFacingRight ? wallJumpForce : -wallJumpForce;
+            rb.linearVelocity = new Vector2(xDirection, jumpForce);
+            hasAirJump = true;
+            state = PlayerState.Airborne;
+        } else if (state == PlayerState.Airborne && hasAirJump) {
+            isFacingRight = !isFacingRight;
+            float xDirection = isFacingRight ? speed : -speed;
+            rb.linearVelocity = new Vector2(xDirection, jumpForce);
+            hasAirJump = false;
         }
-        
+    }
+
+    public void HandleAutoWalk() {
+        if (state == PlayerState.Grounded) {
+            float xDirection = isFacingRight ? speed : -speed;
+            rb.linearVelocity = new Vector2(xDirection, rb.linearVelocity.y);
+        }
     }
 }
